b2d5fef6e9f91860152e08f1ce5a0eb0b4bc78a1
Phatnguyen <trangnguyen.091016@gmail.com>
2026-04-24 10:53:00 +0700

    cau2

--- dequy.py
+++ dequy.py
@@ -3,3 +3,9 @@ def tinh_tong_chu_so(n):
         return n
     return(n%10) + tinh_tong_chu_so(n//10)
 print(tinh_tong_chu_so(345))
+
+def giai_thua(n):
+    if n== 0 or n==1:
+        return n
+    return n * giai_thua(n -1)
+print(giai_thua(5))
