ac184c78c18e7b7badbb49c57886a5cd8a09c92b
Phatnguyen <trangnguyen.091016@gmail.com>
2026-04-24 10:56:06 +0700

    cau3

--- dequy.py
+++ dequy.py
@@ -9,3 +9,10 @@ def giai_thua(n):
         return n
     return n * giai_thua(n -1)
 print(giai_thua(5))
+
+def tinh_luy_thua(a,b):
+    if b == 0:
+        return 1
+    return a * tinh_luy_thua(a, b - 1)
+
+print(tinh_luy_thua(2, 3))
